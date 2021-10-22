d53ea61a110da4745e3c378d65d697585892d484
kaur77263 <kaur77263@gmail.com>
2021-10-22 19:02:32 -0400

    Added main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Rimpaldeep");
+        System.out.println("First change is done by Manpreet");
     }
 }
